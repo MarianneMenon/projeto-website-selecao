cfb7ce00219cff1c878652a70635a8a729c0b9cb
MarianneMenon <marianne.menon@portalsesisp.org.br>
2024-12-09 11:42:06 -0300

    primeira versao

---
+++ v9-lojavirtual - Copia/assets/view/header.php
@@ -0,0 +1,49 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>boodstrap introdução</title>
+
+    <link rel="stylesheet" href="assets/lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/website.css">
+    <link rel="stylesheet" href="assets/lib/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/lib/owl-carousel/owl.theme.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <img id="logotipo" src="assets/images/brasil-logo.png" alt="LogoTipo" width="150px" height="150px">
+        </div>
+
+        <div class="header-black">
+            <div class="container">
+                <ul>
+                    <li>14/09/2024</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <nav id="menu" class="d-flex justify-content-end">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="videos">Vídeos</a></li>
+                         <li><a href="lojavirtual">Loja Virtual</a></li>
+                        <li><a href="noticias">Notícias</a></li>
+                        <li><a href="#">Calendário</a></li>
+                        <li class="search">
+                            <div class="input-group">
+                                <input id="input-search" type="search" class="form-control" placeholder="buscar...">
+                                <span class="input-group-btn">
+                                    <button class="" type="button"><i class="fa fa-search"></i></button>
+                                </span>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+    </header>
